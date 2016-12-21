c63e853e1ca512932168cecd435121dc757022b9
RandomSeeded <nate@blendlabs.com>
2016-12-20 18:39:45 -0800

    solid progress

--- src/island-perimeter/island_perimeter.go
+++ src/island-perimeter/island_perimeter.go
@@ -3,9 +3,29 @@ package main
 import "fmt"
 
 func calculatePerimeter(grid [][]int) int64 {
-  var visited map[string]int
-  visited = make(map[string]int)
-  visited["0,0"] = 1
+  visited := make(map[string]int)
+
+  var startingX int
+  var startingY int
+  for i := 0; i < len(grid); i++ {
+    var breaking bool
+    for j := 0; j < len(grid[i]); j++ {
+      fmt.Println(i, j)
+      if grid[i][j] == 1 {
+        startingX = i
+        startingY = j
+        fmt.Println("starting points set")
+        breaking = true
+        break
+      }
+    }
+    if breaking == true {
+      break
+    }
+  }
+
+  fmt.Println("startingX", startingX)
+  fmt.Println("startingY", startingY)
   
   fmt.Println("visited",visited)
   return 1
